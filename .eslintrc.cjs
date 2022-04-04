d089a4ca34448d0b200b46cefe45be2bbd1fa0e5
scamushka <103003308+scamushka@users.noreply.github.com>
2022-04-04 18:30:38 -0400

    chore: add .eslintrc.cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  rules: {
+    'no-console': 'off',
+    'linebreak-style': ['error', (process.platform === 'win32' ? 'windows' : 'unix')],
+    'arrow-parens': ['error', 'as-needed'],
+    'max-len': 'off',
+    'import/extensions': 'off',
+  },
+};
